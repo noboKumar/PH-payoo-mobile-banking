c712f2b0cb662dd1fd441b174b907d56e4dd801d
noboKumar <nobokumar227@gmail.com>
2025-02-25 17:07:49 +0600

    change font

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,19 @@
-document.getElementById("login-btn").addEventListener("click", function (event) {
-    event.preventDefault()
-  const mobileNum = document.getElementById("mobile-number").value;
-  const pinNum = document.getElementById("pin-number").value;
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const mobileNum = document.getElementById("mobile-number").value;
+    const pinNum = document.getElementById("pin-number").value;
 
-  const convertPinNum = parseInt(pinNum);
+    const convertPinNum = parseInt(pinNum);
 
-  if (mobileNum.length === 11) {
-    if (convertPinNum === 1234) {
-        window.location.href = "./main.html"
-    } else{
-        alert("Wrong Pin Number!")
+    if (mobileNum.length === 11) {
+      if (convertPinNum === 1234) {
+        window.location.href = "./main.html";
+      } else {
+        alert("Wrong Pin Number!");
+      }
+    } else {
+      alert("input a valid mobile number");
     }
-  } else{
-    alert("input a valid mobile number");
-  }
-});
+  });
