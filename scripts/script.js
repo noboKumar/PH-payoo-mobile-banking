ff44ef6df735e2e656a31257fb186499ea68f607
noboKumar <nobokumar227@gmail.com>
2025-02-25 14:05:35 +0600

    add login page

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+document.getElementById("login-btn")
+.addEventListener('click',function(){
+    const mobileNum = document.getElementById("mobile-number");
+    const pinNum = document.getElementById("pin-number");
+
+    const convertMobileNum = parseInt(mobileNum);
+    console.log(typeof mobileNum)
+
+    if (mobileNum.length === 11) {
+
+    }
+})
