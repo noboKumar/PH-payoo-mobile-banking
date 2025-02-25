8cf127d4b74d546441a42fa524d7d00f519565ca
noboKumar <nobokumar227@gmail.com>
2025-02-25 14:19:36 +0600

    add login page script

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
-document.getElementById("login-btn")
-.addEventListener('click',function(){
-    const mobileNum = document.getElementById("mobile-number");
-    const pinNum = document.getElementById("pin-number");
+document.getElementById("login-btn").addEventListener("click", function (event) {
+    event.preventDefault()
+  const mobileNum = document.getElementById("mobile-number").value;
+  const pinNum = document.getElementById("pin-number").value;
 
-    const convertMobileNum = parseInt(mobileNum);
-    console.log(typeof mobileNum)
+  const convertPinNum = parseInt(pinNum);
 
-    if (mobileNum.length === 11) {
-
+  if (mobileNum.length === 11) {
+    if (convertPinNum === 1234) {
+        window.location.href = "./main.html"
+    } else{
+        alert("Wrong Pin Number!")
     }
-})
+  } else{
+    alert("input a valid mobile number");
+  }
+});
